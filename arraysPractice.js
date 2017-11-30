0a7ab6c6959fb87f49f7869af4e1d801619b7d61
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2017-11-29 23:12:40 -0700

    project completed

--- arraysPractice.js
+++ arraysPractice.js
@@ -244,9 +244,9 @@ Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
    let newArray = [];
 
     for( let i = 0; i < arr1.length; i++ ){
-      for( let j = 0; j < arr2.length; j++ ){
+      for( let j = 0; j < arr1.length; j++ ){
         if ( arr1[i] === arr2[j] ) {
-          newArray.push(arr1[i]);
+          newArray.push(arr2[j]);
         }
       }
     }
